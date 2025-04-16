4ee8f75783307048ffa10e3e338e962765ca565a
Jemajr <joshua.ma.udo@gmail.com>
2025-04-16 05:50:29 -0500

    edit hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-class hello {
+class hello { // every program needs an entry and Java programs need a class to host their entry
   public static void main (String[] args) {
     System.out.println("Hello World");
   }
