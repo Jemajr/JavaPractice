384a143ae07283f7bea423d92bc0a5f596d5a9c4
Jemajr <joshua.ma.udo@gmail.com>
2025-04-18 06:13:04 -0500

    edit TenHelloWorlds

--- TenHelloWorlds.java
+++ TenHelloWorlds.java
@@ -1,10 +1,16 @@
 public class TenHelloWorlds {
   public static void main (String[] args) {
-    System.out.print("Hi, ");
-    System.out.print(args[0]);
-    System.out.print(", ");
-    System.out.print(args[1]);
-    System.out.print(", and ");
-    System.out.print(args[2]);
+    // System.out.print("Hi, ");
+    // System.out.print(args[0]);
+    // System.out.print(", ");
+    // System.out.print(args[1]);
+    // System.out.print(", and ");
+    // System.out.print(args[2]);
+
+    int i = 1;
+        while (i <= 10) {
+            System.out.println(i + "th Hello world");
+            i = i + 1;
+        }
   }
 }
