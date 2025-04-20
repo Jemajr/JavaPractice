e27b85c87d62fb7dbef35f1d5e331febd29c7c4a
Jemajr <joshua.ma.udo@gmail.com>
2025-04-20 08:22:57 -0500

    edit Function Growth

--- FunctionGrowth.java
+++ FunctionGrowth.java
@@ -1,7 +1,22 @@
 class FunctionGrowth {
   public static void main (String[] args) {
-    // ln(n) | n | nln(n) | n2 | n3
-    System.out.println("Column1 \t Column2 ");
-    System.out.println("First item \t Second item");
+    // ln(n) | n | nln(n) | n2 | n3 | 2^n
+    // n: 16 -> 2048
+    System.out.println("ln(n) \t n \t nln(n) \t n^2 \t n^3 \t 2^n");
+
+    for (int n = 16; n <= 2048; n *= 2) {
+      System.out.print(Math.log(n));
+      System.out.print("\t");
+      System.out.print(n);
+      System.out.print("\t");
+      System.out.print(n*Math.log(n));
+      System.out.print("\t");
+      System.out.print(Math.pow(n, 2));
+      System.out.print("\t");
+      System.out.print(Math.pow(n, 3));
+      System.out.print("\t");
+      System.out.print(Math.pow(2, n));
+    }
+    
   }
 }
