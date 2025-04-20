1594b51b2ace9fe54e69a7b61777fb8c9d7fbc88
Jemajr <joshua.ma.udo@gmail.com>
2025-04-20 06:20:11 -0500

    Trying out tab spacing

--- FunctionGrowth.java
+++ FunctionGrowth.java
@@ -1,5 +1,7 @@
 class FunctionGrowth {
   public static void main (String[] args) {
-    // starter...
+    // ln(n) | n | nln(n) | n2 | n3
+    System.out.println("Column1 \t Column2 ");
+    System.out.println("First item \t Second item");
   }
 }
