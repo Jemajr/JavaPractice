bf2404f50ac616abc9f66f551a0eefd27a71f34c
Jemajr <joshua.ma.udo@gmail.com>
2025-04-20 05:40:59 -0500

    added initial structure for FivePerLine

--- FivePerLine.java
+++ FivePerLine.java
@@ -3,5 +3,17 @@ class FivePerLine {
     /// one for loop and one if statement
     /// prints the integers from 1000 to 2000
     /// printing five per line
+    /// use % operator
+    /// 
+    /// go from 1000 to 2000
+    /// integers are basically whole numbers 1 2 3 4 5
+    
+    int counter = 1;
+
+    for (int i = 1000; i < 2000; i++) {
+      if (counter % 5 != 0){
+        System.out.print(i + " ");  // added a space to separate numbers
+      }
+    }
   }
 }
