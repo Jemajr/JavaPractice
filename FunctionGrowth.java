54b8d229329aa2f9367b0dccfa3232197293e5ed
Jemajr <joshua.ma.udo@gmail.com>
2025-04-20 08:52:56 -0500

    edit FunctionGrowth header spacing

--- FunctionGrowth.java
+++ FunctionGrowth.java
@@ -1,22 +1,18 @@
 class FunctionGrowth {
   public static void main (String[] args) {
-    // ln(n) | n | nln(n) | n2 | n3 | 2^n
-    // n: 16 -> 2048
-    System.out.println("ln(n) \t n \t nln(n) \t n^2 \t n^3 \t 2^n");
-
-    for (int n = 16; n <= 2048; n *= 2) {
-      System.out.print(Math.log(n));
-      System.out.print("\t");
-      System.out.print(n);
-      System.out.print("\t");
-      System.out.print(n*Math.log(n));
-      System.out.print("\t");
-      System.out.print(Math.pow(n, 2));
-      System.out.print("\t");
-      System.out.print(Math.pow(n, 3));
-      System.out.print("\t");
-      System.out.print(Math.pow(2, n));
-    }
+    System.out.println("log n\tn\tn log n\tn^2\tn^3");
+            for (long n = 2; n <= 2048; n *= 2) {
+              System.out.print((int) Math.log(n));
+              System.out.print('\t');
+              System.out.print(n);
+              System.out.print('\t');
+              System.out.print((int) (n * Math.log(n)));
+              System.out.print('\t');
+              System.out.print(n * n);
+              System.out.print('\t');
+              System.out.print(n * n * n);
+              System.out.println();
+            }
     
   }
 }
