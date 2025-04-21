ac0de9db4755d958e092c2709f2f9db2eff66869
Jemajr <joshua.ma.udo@gmail.com>
2025-04-21 07:38:50 -0500

    add median.java

---
+++ Median.java
@@ -0,0 +1,13 @@
+class Median {
+  public static void main (String[] args){
+    // this program will take in 5 numbers and return the median
+    double first = Integer.parseInt(args[0]);
+    double second = Integer.parseInt(args[1]);
+    double third = Integer.parseInt(args[2]);
+    double fourth = Integer.parseInt(args[3]);
+    double fifth = Integer.parseInt(args[4]);
+
+    // how do I find the 3rd largest value?
+    
+  }
+}
