368e2262421aebd0291369f7771608798397d9a1
Jemajr <joshua.ma.udo@gmail.com>
2025-04-21 07:49:45 -0500

    edited median.java: imported java.util.Arrays

--- Median.java
+++ Median.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
+
 class Median {
   public static void main (String[] args){
     // this program will take in 5 numbers and return the median
-    double first = Integer.parseInt(args[0]);
-    double second = Integer.parseInt(args[1]);
-    double third = Integer.parseInt(args[2]);
-    double fourth = Integer.parseInt(args[3]);
-    double fifth = Integer.parseInt(args[4]);
+    int first = Integer.parseInt(args[0]);
+    int second = Integer.parseInt(args[1]);
+    int third = Integer.parseInt(args[2]);
+    int fourth = Integer.parseInt(args[3]);
+    int fifth = Integer.parseInt(args[4]);
 
-    double[] nums = {first, second, third, fourth, fifth};
-    
+    int[] nums = {first, second, third, fourth, fifth};
+   
+    Arrays.sort(nums);
+
+    System.out.println(nums[2]);
+
   }
 }
