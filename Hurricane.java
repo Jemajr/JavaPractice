24a95aad3d5c1b23051f75e4047fa50fe13e54b5
Jemajr <joshua.ma.udo@gmail.com>
2025-04-21 08:14:16 -0500

    edit Hurricane.java

--- Hurricane.java
+++ Hurricane.java
@@ -1,15 +1,8 @@
-class Hurrican {
+class Hurricane {
   public static void main (String[] args) {
     // this program will take wind speed as an argument and return if it qualifies as a hurrican or not
+    // if it is a hurricane, the program specifies what category it falls under
     int windSpeed = Integer.parseInt(args[0]);
-
-    /*
-     * 1	74 - 95
-2	96 - 110
-3	111 - 130
-4	131 - 155
-5	155 and above
-     */
 
      if (windSpeed < 74) {
       System.out.println("Not a hurricane");
