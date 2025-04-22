9e142cd0610c1535ffb9c24cef97b2eeb682b543
Jemajr <joshua.ma.udo@gmail.com>
2025-04-22 05:00:01 -0500

    adding 1 to the value after multiplying by a factor makes the factor itself included in the range

--- MathRandom.java
+++ MathRandom.java
@@ -3,18 +3,21 @@ class MathRandom {
     /// I'm using this program to understand the range for Math.random() in java
     
     int num1 = (int) Math.random();
-    System.out.println(num1);
+    System.out.println("num1: " + num1);
 
     double num2 = Math.random();
-    System.out.println(num2);
+    System.out.println("num2: " + num2);
 
     double num3 = Math.random() * 10;
-    System.out.println(num3);
+    System.out.println("num3: " + num3);
 
     double num4 = Math.random() * 100;
-    System.out.println(num4);
+    System.out.println("num4: " + num4);
 
     int num5 = (int) (Math.random() * 10);
-    System.out.println(num5);
+    System.out.println("num5: " + num5);
+
+    int num6 = (int) ((Math.random() * 10) + 1);
+    System.out.println("num6: " + num6);
   }
 }
