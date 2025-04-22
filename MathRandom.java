fb7936baa7b52036430976e91c784f2e946b8c3d
Jemajr <joshua.ma.udo@gmail.com>
2025-04-22 04:49:25 -0500

    immediately changing the random value to an integer without multiplying it by another number makes it 0

--- MathRandom.java
+++ MathRandom.java
@@ -1,5 +1,8 @@
 class MathRandom {
   public static void main (String[] args) {
     /// I'm using this program to understand the range for Math.random() in java
+    
+    int num1 = (int) Math.random();
+    System.out.println(num1);
   }
 }
