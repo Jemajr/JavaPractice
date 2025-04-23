1eb71badf32bdb56e41d20731ac21bb144f4867d
Jemajr <joshua.ma.udo@gmail.com>
2025-04-23 05:10:40 -0500

    edit Kickboxer.java

--- KickBoxer.java
+++ KickBoxer.java
@@ -3,5 +3,7 @@ public class KickBoxer {
     // this program will take in a weight as an input and print out the corresponding kickboxing class
     int[] range = {};
     String[] category = {};
+
+    int w = Integer.parseInt(args[0]);  // processes the integer command line argument for weight
   }
 }
