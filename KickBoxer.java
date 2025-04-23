f392a717ac01b97a5515e9a3ee39f95fd4c35c2c
Jemajr <joshua.ma.udo@gmail.com>
2025-04-23 05:09:06 -0500

    setting the basis

--- KickBoxer.java
+++ KickBoxer.java
@@ -1,5 +1,7 @@
 public class KickBoxer {
   public static void main (String[] args) {
     // this program will take in a weight as an input and print out the corresponding kickboxing class
+    int[] range = {};
+    String[] category = {};
   }
 }
