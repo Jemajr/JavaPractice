d619842722ba57153906e4b1325280d19375a2ca
Jemajr <joshua.ma.udo@gmail.com>
2025-04-23 05:14:00 -0500

    edited the weight ranges

--- KickBoxer.java
+++ KickBoxer.java
@@ -1,7 +1,7 @@
 public class KickBoxer {
   public static void main (String[] args) {
     // this program will take in a weight as an input and print out the corresponding kickboxing class
-    int[] range = {};
+    int[] range = {112, 115, 118, 122, 126, 130, 135, 140, 147, 154, 160, 167, 174, 183, 189, 198, 209};
     String[] category = {};
 
     int w = Integer.parseInt(args[0]);  // processes the integer command line argument for weight
