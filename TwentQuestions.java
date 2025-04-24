48c7f93bafd58a9eda26ae6b5879ebcd0098f397
Jemajr <joshua.ma.udo@gmail.com>
2025-04-24 16:10:31 -0500

    edit TwentQuestions.java

--- TwentQuestions.java
+++ TwentQuestions.java
@@ -1,10 +1,17 @@
+import java.util.Scanner; // importing the scanner to read user input
+
 class TwentQuestions {
   public static void main (String[] args) {
+    Scanner cin = new Scanner(System.in);  // creating a scanner object
+
     // this is a guessing game
     int answer = (int) (Math.random()*1000);
     int guess = 0;
 
     while (guess != answer) {
+
+      guess = 
+
       if (guess > answer) {
         System.out.println("Too low");
       } else if (guess < answer) {
