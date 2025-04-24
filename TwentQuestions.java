d3f526567f7012bd40d4b7df5a89888eec18d1f3
Jemajr <joshua.ma.udo@gmail.com>
2025-04-24 16:05:09 -0500

    this is a guessing game

--- TwentQuestions.java
+++ TwentQuestions.java
@@ -1,5 +1,17 @@
 class TwentQuestions {
   public static void main (String[] args) {
     // this is a guessing game
+    int answer = (int) (Math.random()*1000);
+    int guess = 0;
+
+    while (guess != answer) {
+      if (guess > answer) {
+        System.out.println("Too low");
+      } else if (guess < answer) {
+        System.out.println("too low");
+      } else {
+        System.out.println("You win!!");
+      }
+    }
   }
 }
